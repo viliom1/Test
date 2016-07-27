d3489ff86a30e4633e7aafc043890c2bb01e8571
viliom1 <viliom1@gmail.com>
2016-07-27 19:35:15 +0300

    Editted regex

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-	    String pattern = "[a-zA-Z1-9][\\d\\w\\.]{0,}[a-zA-Z-1-9]@[a-z][\\w\\.]+\\.[a-z]{2,3}";
+	    String pattern = "[a-zA-Z1-9][\\d\\w\\.]{0,2}[a-zA-Z-1-9]@[a-z][\\w\\.]+\\.[a-z]{2,3}";
         Arrays.stream(input.split("\\s+")).filter(i -> i.matches(pattern)).forEach(i -> System.out.println(i));
 
     }
